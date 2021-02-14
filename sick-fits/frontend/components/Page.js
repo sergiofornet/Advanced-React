130f35054efa5989763fa7f18c3110823b9ecd02
sergiofornet <sergio.fornet@gmail.com>
2021-02-14 19:36:16 +0100

    Add GlobalStyles component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,13 +1,65 @@
 import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+	@font-face {
+		font-family: 'radnika_next';
+		src: url('/static/radnikanext-medium-webfont.woff2') 
+			 format('woff2');
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	:root {
+		--red: #ff0000;
+		--black: #393939;
+		--grey: #3A3A3A;
+		--gray: var(--grey);
+		--lightGrey: #e1e1e1;
+		--lightGray: var(--lightGrey);
+		--offWhite: #ededed;
+		--maxWidth: 1000px;
+		--bs: 0 13px 24px 0 rgba(0,0,0, 0.09);
+	}
+
+	*, *:before,*:after {
+		box-sizing: inherit;
+	}
+
+	body {
+		font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-size: 1.5rem;
+		line-height: 2;
+		padding: 0;
+		margin: 0;
+	}
+
+	a {
+		text-decoration: none;
+		color: var(--black);
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+
+	button {
+		font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+`;
+
+const InnerStyles = styled.div`
+	max-width: var(--maxWidth);
+	margin: 0 auto;
+	padding: 2em;
+`;
 
 export default function Page({ children, cool }) {
 	return (
 		<div>
+			<GlobalStyles />
 			<Header />
-			<h2>I am the page component</h2>
-			<h3>{cool}</h3>
-			{children}
+			<InnerStyles>{children}</InnerStyles>
 		</div>
 	);
 }
