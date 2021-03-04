69cbd4204cd10a7e5486a694cbcd5522d3e25bdc
sergiofornet <sergio.fornet@gmail.com>
2021-03-04 11:37:30 +0100

    Add page redirect after product creation

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,6 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import DisplayError from './ErrorMessage';
 import { ALL_PRODUCTS_QUERY } from './Products';
@@ -55,10 +56,13 @@ export default function CreateProduct() {
 				try {
 					// Submit the input fields to the backend:
 					// const res = await createProduct(); // This is the same as 'data'
-					// console.log(res);
 					await createProduct();
 					console.log(data);
 					clearForm();
+					// Go to that product's page
+					Router.push({
+						pathname: `/product/${data.createProduct.id}`,
+					});
 				} catch (error) {
 					console.log(error);
 				}
