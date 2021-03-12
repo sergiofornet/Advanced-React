9c4b555f2cb6b508e41b4e4bea9c4a21e46dc38a
sergiofornet <sergio.fornet@gmail.com>
2021-03-12 17:21:22 +0100

    Create Cart component

--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -7,7 +7,23 @@ export const CURRENT_USER_QUERY = gql`
 				id
 				email
 				name
-				# TODO: Query the cart once we have it
+				cart {
+					id
+					quantity
+					product {
+						id
+						price
+						name
+						description
+						photo {
+							id
+							altText
+							image {
+								publicUrlTransformed
+							}
+						}
+					}
+				}
 			}
 		}
 	}
