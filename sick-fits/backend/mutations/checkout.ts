78f406944e291924befeb2dce34c35f5b8b7e4dc
sergiofornet <sergio.fornet@gmail.com>
2021-03-18 13:03:33 +0100

    Create Order & OrderItems in checkout mutation

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -77,6 +77,33 @@ async function checkout(
 		});
 	console.log(charge);
 	// 4. Convert CartItems to OrderItems.
+	const orderItems = cartItems.map((cartItem) => {
+		const orderItem = {
+			name: cartItem.product.name,
+			description: cartItem.product.description,
+			price: cartItem.product.price,
+			quantity: cartItem.quantity,
+			photo: {
+				connect: { id: cartItem.product.photo.id },
+			},
+		};
+		return orderItem;
+	});
+	// 5. Create the order and return it.
+	const order = await context.lists.Order.createOne({
+		data: {
+			total: charge.amount,
+			charge: charge.id,
+			items: { create: orderItems },
+			user: { connect: { id: userId } },
+		},
+	});
+	// 6. Cleanup any old cart item
+	const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+	await context.lists.CartItem.deleteMany({
+		ids: cartItemIds,
+	});
+	return order;
 }
 
 export default checkout;
--- sick-fits/backend/schemas/Order.ts
+++ sick-fits/backend/schemas/Order.ts
@@ -9,16 +9,16 @@ import { list } from '@keystone-next/keystone/schema';
 import formatMoney from '../lib/formatMoney';
 
 export const Order = list({
-	ui: {
-		listView: {
-			initialColumns: ['label', 'user', 'total'],
-		},
-	},
+	// ui: {
+	// 	listView: {
+	// 		initialColumns: ['label', 'user', 'total'],
+	// 	},
+	// },
 	fields: {
 		label: virtual({
 			graphQLReturnType: 'String',
 			resolver(item) {
-				return `WES IS COOL ${formatMoney(item.charge)}`;
+				return `${formatMoney(item.total)}`;
 			},
 		}),
 		total: integer(),
